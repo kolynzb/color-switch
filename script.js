359280d4eb7f13a2533dbbc4ca17be82c1416a7b
kolynzb <collinsbenda360@gmail.com>
2021-09-06 10:10:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".btn")
+const text = document.querySelector('.text > p')
+var number = 0
+btn.addEventListener('click',()=>{
+
+     text.classList.toggle("colorT")
+    
+})
